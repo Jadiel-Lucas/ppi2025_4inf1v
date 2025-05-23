899cf1d8e8c9057203b9aaa88ca5affaf17311d7
Jadiel-Lucas <jadiellucas00@gmail.com>
2025-05-23 18:55:46 +0000

    Aula 23/05/25 - props, children e renderização usando map

--- src/components/MyText.jsx
+++ src/components/MyText.jsx
@@ -1,16 +1,11 @@
 import styles from './MyText.module.css';
 
-export function MyText() {
+export function MyText({title, children}) {
     return (
         <div className={styles.textContainer}>
             <div className={styles.div}>
-                <h1 className={styles.title}>Meu primeiro React App</h1>
-                <p className={styles.text}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, maxime
-                    doloremque distinctio mollitia repudiandae nulla cumque culpa
-                    consequuntur illum, neque saepe voluptates id numquam iste suscipit
-                    totam harum beatae? Cupiditate!
-                </p>
+                <h1 className={styles.title}> {title} </h1>
+                <p className={styles.text}> {children} </p>
             </div>
         </div>
     );
