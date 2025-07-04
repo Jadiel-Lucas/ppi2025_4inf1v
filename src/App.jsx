0efcd04e02babda5e3a3adf30f8a6cba9da3d836
Jadiel-Lucas <jadiellucas00@gmail.com>
2025-07-04 19:03:41 +0000

    aula 04/07

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-{/*import { MyGrid } from "./components/MyGrid";*/}
+import { ProductList } from "./components/ProductList";
+{/*import { MyGrid } from "./components/MyGrid";
 import { MyHeader } from "./components/MyHeader";
-{/*import { MyMain } from "./components/MyMain";
+import { MyMain } from "./components/MyMain";
 import { MyFooter } from "./components/MyFooter";
 import { MyTextList } from "./components/MyTextList";
-import { MyText } from "./components/MyText";*/}
-import { LuckyNumber } from "./components/LuckyNumber";
+import { MyText } from "./components/MyText";
+{/*import { LuckyNumber } from "./components/LuckyNumber";*/}
+import { ProductList } from "./components/ProductList";
 
 
 export default function App() {
   return (
     <>
-        <MyHeader />
-        <LuckyNumber />
+        <ProductList/>
     </>
   );
 }
